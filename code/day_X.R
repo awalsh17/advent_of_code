1957b0486d0764bb6418519f7b45ab9eda18771c
awalsh17 <10963328+awalsh17@users.noreply.github.com>
2021-12-22 17:54:40 -0500

    day22

--- code/day_X.R
+++ code/day_X.R
@@ -1,11 +1,23 @@
 # Day x in R
 
 # Input ----
-input <- readLines(here::here('input/dayx/input.txt'))
+input <- read.table(here::here('input/dayx/input.txt'))
 
 # Part 1 instructions ----
-
+# how many cubes are on?
 # Solve ----
+
+# parse input
+
+# initialize cubes
+cubes <- as.data.frame(expand.grid(y=c(-50:50),y=c(-50:50),z=c(-50:50)))
+cubes$state <- 0
+
+# run steps
+for (i in input) {
+
+}
+# sum
 
 # Part 2 ----
 
