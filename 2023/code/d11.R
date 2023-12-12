076ec4d89ecf087e3153ba505d66dba6c502bec2
awalsh17 <10963328+awalsh17@users.noreply.github.com>
2023-12-11 23:24:51 -0500

    day 11 solutions

--- 2023/code/d11.R
+++ 2023/code/d11.R
@@ -1,15 +1,70 @@
-# day 9
+# day 11
+library(dplyr)
 
-# get data
+# get data and parse it
+source("R/get_input.R")
+input <- get_input("11")
 
+# hm - another grid/graph one? 
 
-solve1 <- function() {
- 
+grid_tidy <- data.frame(
+  row = rep(1:length(input), each = length(input)),
+  col = rep(1:length(input), length(input)),
+  value = strsplit(paste(input, collapse = ""), split = "")[[1]]
+) 
+test <- readLines("2023/inputs/input11test")
+test <- data.frame(
+  row = rep(1:length(test), each = length(test)),
+  col = rep(1:length(test), length(test)),
+  value = strsplit(paste(test, collapse = ""), split = "")[[1]]
+) 
+
+# part 1 ------
+solve1 <- function(input) {
+  # expand the universe 
+  locations <- input |> 
+    group_by(row) |> 
+    mutate(expand_row = all(value == ".")) |> 
+    group_by(col) |> 
+    mutate(expand_col = all(value == ".")) |> 
+    mutate(add_row = cumsum(expand_row)) |> 
+    group_by(row) |> 
+    mutate(add_col = cumsum(expand_col)) |>
+    ungroup() |> 
+    mutate(row_new = row + add_row, col_new = col + add_col) |>
+    filter(value == "#") |> 
+    select(value, row_new, col_new) 
+  
+  # do some dist (manhattan) here
+  dist(locations[, c(2:3)], method = "manhattan") |> sum()
 }
 
-solve1()
+solve1(test) # correct
+solve1(grid_tidy)
 
-solve2 <- function() {
- 
+# part 2 -----
+
+solve2 <- function(input, expand = 1000000) {
+  # expand the universe 
+  locations <- input |> 
+    group_by(row) |> 
+    mutate(expand_row = all(value == ".")) |> 
+    group_by(col) |> 
+    mutate(expand_col = all(value == ".")) |> 
+    mutate(add_row = cumsum(expand_row)) |> 
+    group_by(row) |> 
+    mutate(add_col = cumsum(expand_col)) |>
+    ungroup() |> 
+    mutate(row_new = row + add_row*(expand-1), col_new = col + add_col*(expand-1)) |>
+    filter(value == "#") |> 
+    select(value, row_new, col_new) 
+  
+  # do some dist (manhattan) here
+  dist(locations[, c(2:3)], method = "manhattan") |> sum()
 }
-solve2()
+solve2(test, expand = 100) 
+solve2(grid_tidy, expand = 1000000) 
+
+
+# thoughts: was easier for me to do this tidyverse-y than with matrices
+# part 2 was tricky to figure out the formula to add the rows and columns
--- 2023/code/hanukkah_of_data.R
+++ 2023/code/hanukkah_of_data.R
@@ -39,6 +39,71 @@ all_data[[4]] |>
 
 # day 3? --------
 
-# day 4? --------
+# he was a Cancer born in the year of the Rabbit, so maybe he was able to clean it.
+# spider hat - in same neighborhood as cleaner and has subway
+# male - was shopping around 2017 or 2018
+
+# so need to search for customers born in c(1987, 1999) and with a cancer sign (June 21 - July 22)
+
+suspects <- all_data$customers |> 
+  filter(lubridate::year(birthdate) %in% c(1987, 1999),
+         lubridate::month(birthdate) %in% c(6, 7),
+         (lubridate::month(birthdate) == 6 & lubridate::day(birthdate) > 20) |
+           (lubridate::month(birthdate) == 7 & lubridate::day(birthdate) <23)) |> 
+  tidyr::separate(citystatezip, into = c("city", "statezip"), sep = ",") |> 
+  filter(grepl("NY", statezip))
+
+names_of_suspects <- unique(suspects$name)
+selected_names <- c("Nick Campbell", "Robert Morton", "Derrick Willis",  
+                    "Adrian Brown", "Peter Wilcox", "Todd Simmons", "Sam Greene")
+suspects |> left_join(all_data$orders, by = "customerid") |> 
+  inner_join(all_data$order_items, by = "orderid") |> 
+  inner_join(all_data$products, by = "sku") |> 
+  # filter by names
+  filter(name %in% selected_names) |> 
+  filter(desc == "Rug Cleaner") 
+
+# picked the person who bought rug cleaner - that was correct
+
+# day 4 --------
+# now we are looking for a woman that bought pastries before 5 am. 
+# it would be a few weeks after the last person bought rug cleaner - 2017-07-22
+
+# I dont have a list of pastries so lets filter by order date
+suspects <- all_data$customers |> 
+  # tidyr::separate(citystatezip, into = c("city", "statezip"), sep = ",") |> 
+  # filter(grepl("NY", statezip)) |> 
+  inner_join(all_data$orders, by = "customerid") |> 
+  filter(lubridate::hour(shipped) <= 5)
+  
+suspects |> 
+  inner_join(all_data$order_items, by = "orderid") |> 
+  inner_join(all_data$products, by = "sku") |> 
+  filter(grepl("BKY", sku)) |> View()
+
+unique(suspects$name)
+
+# should we pick the people that also bought rug cleaner? bike kit?
+# my first guess was wrong - even thought she bought rug cleaner
+# I needed someone that had ordered more than ONE pastry
+
+# day 5 -----
+
+# staten island, woman, 10-11 Senior Cats, had to have ordered something pre 2018
+
+cat_products <- all_data$products |> 
+  filter(grepl("Senior Cat", desc))
 
 
+suspects <- all_data$customers |> 
+  filter(grepl("Staten Island", citystatezip)) |> 
+  inner_join(all_data$orders, by = "customerid") |> 
+  inner_join(all_data$order_items, by = "orderid") |> 
+  inner_join(cat_products, by = "sku")
+
+# who had the most?
+count(suspects, name, phone, sort = T)
+
+# picked the top one and it was correct
+
+# day 6 ------
