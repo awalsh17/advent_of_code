69fe9259cd6c059426baf6767a890e53796e324d
awalsh17 <10963328+awalsh17@users.noreply.github.com>
2023-12-15 18:10:23 -0500

    part 1 of day 12

--- 2023/code/d12.R
+++ 2023/code/d12.R
@@ -1,5 +1,7 @@
-# day 9
+# day 12
 
+library(dplyr)
+library(gtools)
 # get data and parse it
 source("R/get_input.R")
 input <- data.frame(start = get_input(12)) |> 
@@ -8,6 +10,26 @@ test <- data.frame(start = readLines("2023/inputs/input12")) |>
   tidyr::separate(start, into = c("clue", "vals"), sep = " ")
 
 # part 1 ------
+
+# get the number of possible ways for a clue to make a value
+#' example: get_possible("?###????????", "3,2,1")
+get_possible <- function(clue, value) {
+  # brute force every combination of . or # for the ? in the clue
+  clue_vector <- strsplit(clue, "")[[1]]
+  n_qs <- sum(clue_vector == "?")
+  all_options <- permutations(2, n_qs, c(".", "#"), repeats.allowed = TRUE)
+  all_new_values <- apply(all_options, 1, function(x) {
+    # replace the ? with the new values
+    new_clue <- clue_vector
+    new_clue[new_clue == "?"] <- x
+    paste(
+      nchar(stringr::str_extract_all(paste0(new_clue, collapse = ""), "#+")[[1]]),
+      collapse = ",")
+  })
+  # how many of the new values match the value?
+  sum(all_new_values == value)
+}
+
 solve1 <- function(input) {
 
   results = input |> 
@@ -18,34 +40,24 @@ solve1 <- function(input) {
     mutate(clue_len = nchar(clue)) |> 
     rowwise() |> 
     mutate(
-      q_count = stringr::str_count(clue, "\\?"),
-      dot_count = stringr::str_count(clue, "\\."),
-      hash_count = stringr::str_count(clue, "#"),
-      val_count = stringr::str_count(vals, ",") + 1,
+      val_count = stringr::str_count(vals, ","),
       val_len = eval(parse(text = gsub(",","+", vals)))) |>
     ungroup() |> 
     mutate(solutions = case_when(
-      clue_len <= (val_count + val_len - 1) ~ 1,
-      TRUE ~ clue_len - (val_count + val_len - 1)
-    )) |> # the 1s are correct, but > 1 are wrong
-    # this is wrong, but just getting a placeholder
-    
-    mutate(solutions = case_when(
-      solutions > 1 & hash_count == 0 ~ dot_count + 1,
-      solutions > 1 & dot_count > 0 ~ dot_count * dot_count,
-      solutions > 1  ~ q_count + 1,
-      TRUE ~ solutions
-    )) 
+      clue_len <= (val_count + val_len) ~ 1,
+      TRUE ~ 2
+    )) # the 1s are correct, but > 1 are wrong
+  solutions_one <- results[results$solutions == 1,]
+  solutions <- apply(results[results$solutions > 1,], 1, function(x) {
+    get_possible(x["clue"], x["vals"])
+  })
   # then test how many of the options meet the vals criteria
   # then return the sum of the vals for the options that meet the criteria
-  sum(results$solutions)
+  sum(solutions) + nrow(solutions_one)
 }
 
-# write a function that given a clue (string), return the possible combinations
-
-
-solve1(test)
-solve1(input) # 4056 is low
+solve1(test) # 21
+solve1(input) # 7490
 
 # part 2 ------
 solve2 <- function() {
@@ -54,9 +66,6 @@ solve2 <- function() {
 solve2()
 
 # deep thoughts =====
-# We can reduce the string some and we find the are almost all unique.
-# We can resolve a lot of the rows as having only one solution
-# That reduces the search space to the other rows
-
-# I def need something more clever here - some trick like recursion or memoisation
-# we can then search through a bunch of different options without repeating
+# I def need something more clever here 
+# I looked at hints to see that I will need recursive function with memoisation
+# instead of brute force
