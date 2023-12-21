a548a480e321f884ab62dddc9afc82a35438fa66
awalsh17 <10963328+awalsh17@users.noreply.github.com>
2023-12-21 17:31:29 -0500

    day17 not started

---
+++ 2023/code/d17.R
@@ -0,0 +1,13 @@
+# day 17
+
+# get the data -----
+
+# make a graph
+
+# part 1 ------
+
+# part 2 ------
+
+# deep thoughts ------
+# shortest path with edge weights seems to be the way. Adding in the complexity
+# that you cannot go more than three steps in one direction
